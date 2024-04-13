94123bc7eafd62ef1e2e84485c309e529cc8c3f6
anderu <andrewtan1310@gmail.com>
2024-04-13 17:45:26 +0800

    Use react-hook-form and update mobile design

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -3,50 +3,103 @@ import desktopImg from '../assets/illustration-sign-up-desktop.svg';
 import iconList from '../assets/icon-list.svg';
 import iconSuccess from '../assets/icon-success.svg';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 export default function Newsletter() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: '',
+    },
+  });
   const [email, setEmail] = useState('');
+  const [showTPage, setShowTPage] = useState(false);
+
+  const onSubmit = (data) => {
+    console.log(data.email);
+    setEmail(data.email);
+    console.log(email);
+    setShowTPage(!showTPage);
+  };
 
   return (
-    <div className='w-full'>
-      <div>
-        <img src={mobileImg} alt='' className='w-full' />
-      </div>
-      <div className='px-5 py-10 flex flex-col gap-6 text-slate-800'>
-        <h2 className='text-4xl font-semibold'>Stay updated!</h2>
-        <p>Join 60,000+ product managers receiving monthly updates on:</p>
-        <ul>
-          <li className='flex gap-3 items-start mb-2'>
-            <img src={iconList} />
-            Product discovery and building what matters
-          </li>
-          <li className='flex gap-3 items-start mb-2'>
-            <img src={iconList} />
-            Measuring to ensure updates are a success
-          </li>
-          <li className='flex gap-3 items-start'>
-            <img src={iconList} />
-            And much more!
-          </li>
-        </ul>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <label htmlFor='email' className='flex flex-col gap-2'>
-            <span className='text-xs font-bold'>Email address</span>
-            <input
-              type='email'
-              id='email'
-              name='email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder='email@company.com'
-              className='border-[1px] border-slate-400 placeholder-slate-400 p-4 rounded-lg mb-5 '
-            />
-          </label>
-          <button className='bg-slate-800 text-white w-full py-4 rounded-lg font-semibold'>
-            Subscribe to monthly newsletter
+    <>
+      {!showTPage && (
+        <div className='w-full'>
+          <div>
+            <img src={mobileImg} alt='' className='w-full' />
+          </div>
+          <div className='px-5 py-10 flex flex-col gap-6 text-slate-800'>
+            <h2 className='text-4xl font-semibold'>Stay updated!</h2>
+            <p>Join 60,000+ product managers receiving monthly updates on:</p>
+            <ul>
+              <li className='flex gap-3 items-start mb-2'>
+                <img src={iconList} />
+                Product discovery and building what matters
+              </li>
+              <li className='flex gap-3 items-start mb-2'>
+                <img src={iconList} />
+                Measuring to ensure updates are a success
+              </li>
+              <li className='flex gap-3 items-start'>
+                <img src={iconList} />
+                And much more!
+              </li>
+            </ul>
+            <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col'>
+              <label htmlFor='email' className='text-xs font-bold mb-2'>
+                Email Address
+              </label>
+              <input
+                id='email'
+                {...register('email', {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                })}
+                placeholder='email@company.com'
+                className='border-[1px] border-slate-400 placeholder-slate-400 p-4 rounded-lg mb-5 '
+              />
+              {errors.email && (
+                <span className='-mt-5 mb-5 text-red-500 text-sm font-semibold'>
+                  Invalid email format
+                </span>
+              )}
+
+              <input
+                type='submit'
+                className='bg-slate-800 text-white w-full py-4 rounded-lg font-semibold hover:bg-red-400'
+                value='Subscribe to monthly newsletter'
+              />
+            </form>
+          </div>
+        </div>
+      )}
+      {showTPage && (
+        <div className='px-8 py-10 flex flex-col w-full'>
+          <img
+            src={iconSuccess}
+            alt=''
+            className='h-14 self-start mb-8 mt-12'
+          />
+          <h2 className='text-4xl font-semibold mb-5'>
+            Thanks for subscribing!
+          </h2>
+          <p>
+            A confirmation email has been sent to
+            <span className='font-semibold'> {email}</span>. Please open it and
+            click the button inside to confirm your subscription.
+          </p>
+          <button
+            onClick={() => setShowTPage(!showTPage)}
+            className='bg-slate-800 text-white  py-4 rounded-lg font-semibold hover:bg-red-400 fixed bottom-10 right-5 left-5'
+          >
+            Dismiss message
           </button>
-        </form>
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 }
