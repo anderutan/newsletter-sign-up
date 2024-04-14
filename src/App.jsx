b9209a751b3ee5479ca03920c4ae138a5aad8899
anderu <andrewtan1310@gmail.com>
2024-04-15 07:18:15 +0800

    Update desktop version

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Newsletter from './components/Newsletter';
 
 function App() {
   return (
-    <div className='w-full'>
+    <div className='w-full flex justify-center items-center sm:bg-slate-700 sm:h-screen'>
       <Newsletter />
     </div>
   );
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -4,8 +4,10 @@ import iconList from '../assets/icon-list.svg';
 import iconSuccess from '../assets/icon-success.svg';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import useWindowDimensions from '../utility/UseWindowDimensions';
 
 export default function Newsletter() {
+  const { width } = useWindowDimensions();
   const {
     register,
     handleSubmit,
@@ -28,12 +30,18 @@ export default function Newsletter() {
   return (
     <>
       {!showTPage && (
-        <div className='w-full'>
-          <div>
-            <img src={mobileImg} alt='' className='w-full' />
+        <div className='w-full max-w-[450px] sm:max-w-[800px] sm:m-5 sm:grid sm:grid-cols-2 sm:gap-10 sm:items-center sm:p-5 sm:bg-white sm:rounded-xl'>
+          <div className='sm:col-start-2 sm:row-start-1'>
+            {width < 640 ? (
+              <img src={mobileImg} alt='' className='w-full' />
+            ) : (
+              <img src={desktopImg} alt='' className='w-full max-h-[550px]' />
+            )}
           </div>
-          <div className='px-5 py-10 flex flex-col gap-6 text-slate-800'>
-            <h2 className='text-4xl font-semibold'>Stay updated!</h2>
+          <div className='px-5 py-10 flex flex-col gap-6 text-slate-800 sm:col-start-1'>
+            <h2 className='text-4xl font-semibold sm:text-5xl'>
+              Stay updated!
+            </h2>
             <p>Join 60,000+ product managers receiving monthly updates on:</p>
             <ul>
               <li className='flex gap-3 items-start mb-2'>
@@ -70,7 +78,7 @@ export default function Newsletter() {
 
               <input
                 type='submit'
-                className='bg-slate-800 text-white w-full py-4 rounded-lg font-semibold hover:bg-red-400'
+                className='bg-slate-800 text-white w-full py-4 rounded-lg font-semibold hover:bg-gradient-to-r from-rose-500 to-orange-500'
                 value='Subscribe to monthly newsletter'
               />
             </form>
@@ -78,13 +86,13 @@ export default function Newsletter() {
         </div>
       )}
       {showTPage && (
-        <div className='px-8 py-10 flex flex-col w-full'>
+        <div className='text-slate-800 px-8 py-10 flex flex-col w-full h-screen   bg-white sm:max-w-[450px] sm:max-h-[450px] sm:rounded-xl sm:p-12'>
           <img
             src={iconSuccess}
             alt=''
-            className='h-14 self-start mb-8 mt-12'
+            className='h-14 self-start mb-8 mt-12 sm:mb-8 sm:mt-0'
           />
-          <h2 className='text-4xl font-semibold mb-5'>
+          <h2 className='text-4xl font-semibold mb-5 sm:text-5xl'>
             Thanks for subscribing!
           </h2>
           <p>
@@ -94,7 +102,7 @@ export default function Newsletter() {
           </p>
           <button
             onClick={() => setShowTPage(!showTPage)}
-            className='bg-slate-800 text-white  py-4 rounded-lg font-semibold hover:bg-red-400 fixed bottom-10 right-5 left-5'
+            className='bg-slate-800 text-white mt-auto py-4 rounded-lg font-semibold hover:bg-gradient-to-r from-rose-500 to-orange-500 '
           >
             Dismiss message
           </button>
